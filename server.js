27a61bd46484be4d9e05c5c73e903230424c8eb5
yojina <yojinaaryal2064@gmail.com>
2025-10-27 14:21:10 +0545

    day 2.3

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const fs=require("fs");
 const url = require("url");
 
 const myServer=http.createServer(( req,res)=>{
-    if (req.url=="/favicon.ico") return res.end;
-    const log=`${Date.now()}:${req.url} new req received \n`;
+    if (req.url=="/favicon.ico") return res.end();
+    const log=`:${req.url} new req received \n`;
     const myUrl=url.parse(req.url,true);
     console.log(myUrl);
   fs.appendFile("log.txt",log,(err,data)=>
