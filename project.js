13e9af9e3bdc64668e12213a7804ba3a6a68410a
yojina <yojinaaryal2064@gmail.com>
2025-11-01 21:24:49 +0545

    day 5.3

--- project.js
+++ project.js
@@ -1,58 +1,22 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const mongoose = require("mongoose");
 
-const users = require("./MOCK_DATA.json");
-const { default: mongoose } = require("mongoose");
-const { stringify } = require("querystring");
-const { type } = require("os");
+const users = require("./MOCK_DATA.json")
 const port = 3000;
-
-//connection
-mangoose.connect ("mongodb://127.0.0.1:27017/youtube-app-1")
-.then (()=>
-    console.log("MongoDB Connected"))
-.catch(err =>console.log("Mongo error",err));
-
-
-//schema
-const userSchema = new mangoose.Schema({
-    first_name: {
-        type: String,
-        required: true,
-    },
-    last_name: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    job_title: {
-        type: String,
-    },
-    gender:
-    {
-        type: String,
-    }
-})
-const User = mangoose.model("user", userSchema);
-
 //midlleware-plugin
 app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
-    fs.appendFile(
-        "log.txt",
-        `\n${Date.now()}: ${req.method} ${req.path} from ${req.ip}`,
-        (err) => {
-            if (err) {
-                console.error("Error writing to log file:", err);
-            }
-            next();
-        }
-    );
+  fs.appendFile(
+    "log.txt",
+    `\n${Date.now()}: ${req.method} ${req.path} from ${req.ip}`,
+    (err) => {
+      if (err) {
+        console.error("Error writing to log file:", err);
+      }
+      next(); 
+    }
+  );
 });
 
 
@@ -70,50 +34,43 @@ app.get('/users', (req, res) => {
 //always add X to custom headers
 
 app.get('/api/users/', (req, res) => {
-    res.setHeader("X-myName", "Yojina aryal");
-
-    return res.json(user);
+    res.setHeader("X-myName","Yojina aryal");
+    
+   return res.json(user);
 })
 
 app.route('/api/users/:id')
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        if (!user) {
-            return res.status(404).json({ error: "YOUR USER ID IS BEYOND THE DATA" })
-        }
-        return res.json(user);
-    }).patch((req, res) => {
-        //todo : to edit id
-        return res.json({ status: "pendingg" });
-    }).delete((req, res) => {
-        //todo : to delete id
-        return res.json({ status: "pendingg" });
-    })
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    if (!user)
+    {return res.status(404).json({error: "YOUR USER ID IS BEYOND THE DATA"})
+}
+    return res.json(user);
+}).patch((req, res) => {
+    //todo : to edit id
+    return res.json({ status: "pendingg" });
+}).delete((req, res) => {
+    //todo : to delete id
+    return res.json({ status: "pendingg" });
+})
 
 
 
-app.post('/api/users', async (req, res) => {
+app.post('/api/users', (req, res) => {
     const body = req.body;
-    if (!body || !body.first_name || !body.email || !body.job_title) {
-        return res.status(400).json({ msg: "ALL FIELDS ARE REQUIRED" });
+    if(!body||!body.first_name ||!body.email ||!body.job_title )
+    {
+        return res.status(400).json({msg:"ALL FIELDS ARE REQUIRED"});
     }
-
+    users.push({...body,id: users.length +1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({ status: "pendingg" });
+    } )
     
-    
-    const result= await User.create({
-        firstname: body.first_name,
-        email: body.email,
-        gender: body.gender,
-        job_title: body.job_title,
-    });
-    console.log("result",result);
-    return res.status(201).json({msg: "success"});
-
-});
-
+})
 
 
 app.listen(port, () => {
-    console.log(`SERVER STARTED AT PORT:  ${port}`);
+    console.log(`SERVER STARTED AT PORT: ${port}`);
 })
